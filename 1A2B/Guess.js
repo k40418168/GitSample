7d3b62f3a1ee25f0e377f63a20dfddbb9bb54545
Shrank Kao <a23057475@gmail.com>
2020-12-30 14:45:28 +0800

    1A2B(無防呆)

--- 1A2B/Guess.js
+++ 1A2B/Guess.js
@@ -2,9 +2,13 @@ let start = document.getElementById("start");
 let restart = document.getElementById("restart");
 let cheat = document.getElementById("cheat");
 let _answer = [];
+let guess = document.getElementById("guess");
+guess.setAttribute("disabled",true);
+restart.setAttribute("disabled",true);
+cheat.setAttribute("disabled",true);
 
 function randomNum() {
-    answer = [];
+    _answer = [];
     for(i=0;i<4;i++){
         let number = Math.floor(Math.random()*10);
         if(!_answer.includes(number)){
@@ -19,35 +23,69 @@ start.onclick = function(){
     randomNum();
     start.setAttribute("disabled",true);
     clearInfo();
+    guess.removeAttribute("disabled");
+    cheat.removeAttribute("disabled");
+    restart.removeAttribute("disabled");
 }
 cheat.onclick = function () {
+    if(cheat.hasAttribute("disabled")){
+        return;
+    }
     alert(`${_answer.join('')}`);
 }
 function clearInfo (){
     document.getElementById("guessResults").innerHTML = "";
 }
 restart.onclick = function(){
+    if(restart.hasAttribute("disabled")){
+        return;
+    }
     clearInfo();
     randomNum();
+}
+guess.onclick = function (event) {
+    if(guess.hasAttribute("disabled")){
+        return;
+    }
+    Comparison();
+    OutResult();
+    
 }
 let A = 0,B = 0;
 let userGuess = "";
 let _userGuess =[];
 let result,notNum;
-
 function Comparison(){
-    userGuess = document.getElementById("userGuess").Value;
-    _userGuess = userGuess.split("");
+    userGuess = document.getElementById("userGuess").value;
+    _userGuess = userGuess.split('');
+    A=0,B=0;
     for(i=0;i<4;i++){
         if(_answer[i] == _userGuess[i]){
             A++;
         }
-        if(_userGuess.includes(parseInt(_answer[i]))){
+        if(_answer.includes(parseInt(_userGuess[i]))){
             B++;
         }
     }
+    B=B-A;
 }
 
 function OutResult(){
+    let liGuess = document.createElement("li");
+    liGuess.setAttribute("class","list-group-item");
+    let ulGuess = document.getElementById("guessResults");
+    let spanGuess = document.createElement("span");
+    if(A==4){
+        spanGuess.setAttribute("class","label label-success");
+        spanGuess.textContent = (`${A}A${B}B`);
+        alert("你猜對了");
+        guess.setAttribute("disabled",true);
+    }
+    else{
+        spanGuess.setAttribute("class","label label-danger");
+        spanGuess.textContent = (`${A}A${B}B`);
+    }
     
+    liGuess.append(spanGuess,` ${userGuess}`);
+    ulGuess.append(liGuess);
 }
